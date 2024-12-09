cbc3d0a4aae0725d84285a8de026947aeeed72cf
Isaque Caetano Nascimento <ggomesnascimento123@gmail.com>
2024-12-09 17:45:44 -0300

    Adicionar arquivos de projeto.

---
+++ Trabalho AEDS 2024/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Trabalho_AEDS_2024
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
